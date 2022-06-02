a3bbbbf3aef60809bbe2f36a9a35aafa516433ac
jbmahaffey <84487961+jbmahaffey@users.noreply.github.com>
2022-06-02 11:34:56 -0700

    Update cpu.py

--- cpu.py
+++ cpu.py
@@ -12,7 +12,7 @@ requests.packages.urllib3.disable_warnings()
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--fortigate', default='jmahaffey-api-test.fortidemo.fortinet.com:10403', help='Firewall IP Address')
-    parser.add_argument('--token', default='ckgn4cjHGg1H05513jn7G57Qw3wqcj', help='API Token')
+    parser.add_argument('--token', default='', help='API Token')
     parser.add_argument('--interval', default='1', help='Time period to query cpu stats 1-min, 10-min, 30-min, 1-hour, 12-hour')
     parser.add_argument('--devlist', default='cpu.csv', help='CPU usage over time.')
     parser.add_argument('--version', default='6.4', help='Fortigate version')
